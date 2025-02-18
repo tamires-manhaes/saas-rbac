3c4d439b5dd535c864b436ad6f5872b7bc1ffc29
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2025-02-18 12:17:45 -0300

    refactor: remove deprecated error handler and update import paths for consistency

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,9 +1,9 @@
 import fastifyCors from '@fastify/cors'
-import fastifyJWT from '@fastify/jwt'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import { env } from '@saas/env'
-import { fastify } from 'fastify'
+import fastify from 'fastify'
 import {
   jsonSchemaTransform,
   serializerCompiler,
@@ -11,15 +11,37 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
-import { errorHandler } from './error-handler'
-import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
-import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
+import { errorHandler } from '@/http/error-handler'
+import { authenticateWithGithub } from '@/http/routes/auth/authenticate-with-github'
+import { authenticateWithPassword } from '@/http/routes/auth/authenticate-with-password'
+import { getProfile } from '@/http/routes/auth/get-profile'
+import { requestPasswordRecover } from '@/http/routes/auth/request-password-recover'
+import { resetPassword } from '@/http/routes/auth/reset-password'
+import { getOrganizationBilling } from '@/http/routes/billing/get-organization-billing'
+import { acceptInvite } from '@/http/routes/invites/accept-invite'
+import { createInvite } from '@/http/routes/invites/create-invite'
+import { getInvite } from '@/http/routes/invites/get-invite'
+import { getPendingInvites } from '@/http/routes/invites/get-pending-invites'
+import { rejectInvite } from '@/http/routes/invites/reject-invite'
+import { revokeInvite } from '@/http/routes/invites/revoke-invite'
+import { getMembers } from '@/http/routes/members/get-members'
+import { removeMember } from '@/http/routes/members/remove-member'
+import { updateMember } from '@/http/routes/members/update-member'
+import { createOrganization } from '@/http/routes/orgs/create-organization'
+import { getMembership } from '@/http/routes/orgs/get-membership'
+import { getOrganization } from '@/http/routes/orgs/get-organization'
+import { getOrganizations } from '@/http/routes/orgs/get-organizations'
+import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
+import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
+import { updateOrganization } from '@/http/routes/orgs/update-organization'
+import { createProject } from '@/http/routes/projects/create-project'
+import { deleteProject } from '@/http/routes/projects/delete-project'
+import { getProject } from '@/http/routes/projects/get-project'
+import { getProjects } from '@/http/routes/projects/get-projects'
+import { updateProject } from '@/http/routes/projects/update-project'
+
 import { createAccount } from './routes/auth/create-account'
-import { getProfile } from './routes/auth/get-profile'
-import { requestPasswordRecover } from './routes/auth/request-password-recovery'
-import { resetPassword } from './routes/auth/reset-password'
-import { createOrganization } from './routes/orgs/create-organization'
-import { getMembership } from './routes/orgs/get-membership'
+import { getInvites } from './routes/invites/get-invites'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -31,8 +53,8 @@ app.setErrorHandler(errorHandler)
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Next.js SaaS RBAC',
-      description: 'Full-stack SaaS app with multi-tenant & RBAC.',
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS with multi-tenant & RBAC.',
       version: '1.0.0',
     },
     components: {
@@ -52,21 +74,47 @@ app.register(fastifySwaggerUI, {
   routePrefix: '/docs',
 })
 
-app.register(fastifyJWT, {
+app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
 app.register(fastifyCors)
+
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
-app.register(authenticateWithGithub)
 
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
+app.register(updateOrganization)
+app.register(shutdownOrganization)
+app.register(transferOrganization)
 
-app.listen({ port: env.SERVER_PORT }).then(() => {
-  console.log('http server running on http://localhost:3333!')
+app.register(createProject)
+app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
+app.register(updateProject)
+
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
+
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
+app.register(getPendingInvites)
+
+app.register(getOrganizationBilling)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
 })
