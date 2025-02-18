a224447aa8db81374530244738975aa538540dc0
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2025-02-18 16:14:38 -0300

    feat: add sign-up page with form fields for name, email, password, and GitHub sign-up option

---
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,50 @@
+import { Github } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
+      </div>
+
+      <Button type="submit" className="w-full">
+        Create account
+      </Button>
+
+      <Button variant="link" className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" variant="outline" className="w-full">
+        <Github className="dark size-4" /> Sign up with Github
+      </Button>
+    </form>
+  )
+}
