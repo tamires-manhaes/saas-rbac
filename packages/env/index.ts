e79ed5c949be84d48e1c9a6fc24895c0d17dab89
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2025-02-18 12:16:27 -0300

    feat: add tsup configuration and update environment variables for improved build process

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,23 +3,28 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    SERVER_PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string().url(),
+
     JWT_SECRET: z.string(),
+
     GITHUB_OAUTH_CLIENT_ID: z.string(),
-    GITHUB_OAUTH_SECRET: z.string(),
+    GITHUB_OAUTH_CLIENT_SECRET: z.string(),
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.string().url(),
   },
   client: {},
-  shared: {},
+  shared: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
   runtimeEnv: {
-    SERVER_PORT: process.env.SERVER_PORT,
+    PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
-    GITHUB_OAUTH_SECRET: process.env.GITHUB_OAUTH_SECRET,
+    GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
 })
