8eff7bc7cfc93859448ee322f9b39424072d591a
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2025-02-18 11:52:42 -0300

    feat: add getUserMembership method to FastifyRequest for organization membership retrieval

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnathorizedError } from '../routes/_errors/unathorized-error'
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
@@ -11,6 +13,30 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
         return sub
       } catch {
         throw new UnathorizedError('Invalid auth token')
+      }
+    }
+
+    request.getUserMembership = async (slug: string) => {
+      const userId = await request.getCurrentUserId()
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug,
+          },
+        },
+        include: { organization: true },
+      })
+
+      if (!member) {
+        throw new UnathorizedError(`You're not a member of this organization.`)
+      }
+
+      const { organization, ...membership } = member
+
+      return {
+        organization,
+        membership,
       }
     }
   })
