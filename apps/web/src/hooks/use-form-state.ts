62909f4046fc07d536df0900f5945e041fd2ee2c
Tamires <tamiresmanhaes4@gmail.com>
2025-02-20 18:46:42 -0300

    feat: add custom hook useFormState for managing form submission and state

---
+++ apps/web/src/hooks/use-form-state.ts
@@ -0,0 +1,37 @@
+import { FormEvent, useState, useTransition } from 'react'
+
+interface FormState {
+  sucess: boolean
+  message: string | null
+  errors: Record<string, string[]> | null
+}
+
+export function useFormState(
+  action: (data: FormData) => Promise<FormState>,
+  onSuccess: () => Promise<void> | void,
+  initialState?: FormState,
+) {
+  const [isPending, startTransition] = useTransition()
+
+  const [formState, setFormState] = useState(
+    initialState ?? { sucess: false, message: null, errors: null },
+  )
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const state = await action(data)
+
+      if (state.sucess === true && onSuccess) {
+        await onSuccess()
+      }
+      setFormState(state)
+    })
+  }
+
+  return [formState, handleSubmit, isPending] as const
+}
