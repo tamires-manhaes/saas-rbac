d39c6b86617477a910ef446b111de72bed2e08f5
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-02-21 18:57:33 -0300

    feat: update Home component to display authenticated user information

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <h1>salve</h1>
-    </div>
-  )
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
