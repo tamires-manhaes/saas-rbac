0b9adaa2688446443638a6e376e266a41bcae81d
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-02-21 18:56:57 -0300

    feat: enhance authentication flow with token validation and user profile retrieval

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,5 +1,25 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
-  return !!(await cookies()).get('token')?.value
+  const cookieStore = await cookies()
+  return !!cookieStore.get('token')?.value
+}
+
+export async function auth() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+    return { user }
+  } catch (err) {}
+
+  redirect('/')
 }
