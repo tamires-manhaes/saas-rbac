d942eb29442ffe1b42a26cde446fbfd3121f1da4
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-02-22 18:12:39 -0300

    feat: add profile button component with user information and sign-out option

---
+++ apps/web/src/components/profile-button.tsx
@@ -0,0 +1,44 @@
+import { ChevronDown, LogInIcon } from 'lucide-react'
+
+import { auth } from '@/auth/auth'
+import { getInitials } from '@/lib/getInitials'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export async function ProfileButton() {
+  const { user } = await auth()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center gap-3 outline-none">
+        <div className="flex flex-col items-end">
+          <span className="text-sm font-medium">{user.name}</span>
+          <span className="text-xs text-muted-foreground">{user.email}</span>
+        </div>
+
+        <Avatar>
+          {user?.avatarUrl && (
+            <AvatarImage src={user?.avatarUrl} alt="Tamires Pimenta" />
+          )}
+          {user.name && (
+            <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+          )}
+        </Avatar>
+        <ChevronDown className="size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem asChild>
+          <a href="/api/auth/sign-out">
+            <LogInIcon className="mr-2 size-4" />
+            Sign out
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
