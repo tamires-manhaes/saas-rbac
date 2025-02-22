13f2413600f5387217a9032d80a3a934f5483701
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-02-22 18:11:44 -0300

    feat: implement sign-out functionality by deleting user token and redirecting to sign-in page

--- apps/web/src/app/api/auth/sign-out/route.ts
+++ apps/web/src/app/api/auth/sign-out/route.ts
@@ -1,31 +1,14 @@
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
-import { signInWithGithub } from '@/http/sign-in-with-github'
-
 export async function GET(request: NextRequest) {
-  const code = request.nextUrl.searchParams.get('code')
-
-  if (!code) {
-    return NextResponse.json(
-      { message: 'Github OAuth  code was not found.' },
-      { status: 400 },
-    )
-  }
-
-  const { token } = await signInWithGithub({ code })
-
-  await cookies().then((c) => {
-    c.set('token', token, {
-      path: '/',
-      maxAge: 60 * 60 * 24 * 7, // 7days
-    })
-  })
-
   const redirectUrl = request.nextUrl.clone()
 
-  redirectUrl.pathname = '/'
-  redirectUrl.search = ''
+  redirectUrl.pathname = '/auth/sign-in'
+
+  await cookies().then((c) => {
+    c.delete('token')
+  })
 
   return NextResponse.redirect(redirectUrl)
 }
