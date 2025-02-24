7e3489aeb04867ee00cf6244804b9e75abbc6bdb
Tamires <tamiresmanhaes4@gmail.com>
2025-02-24 15:03:36 -0300

    feat: add getMembership function to retrieve organization membership details

---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,19 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetMembershipResponse {
+  memberhsip: {
+    id: string
+    role: Role
+    userId: string
+    organizationId: string
+  }
+}
+
+export async function getMembership(org: string) {
+  const result = await api
+    .get(`organization/${org}/membership`)
+    .json<GetMembershipResponse>()
+  return result
+}
