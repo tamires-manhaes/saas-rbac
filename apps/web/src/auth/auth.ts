334d07668d7ca231fb8a6cbaa30012b95f0723f6
Tamires <tamiresmanhaes4@gmail.com>
2025-02-24 15:03:43 -0300

    feat: add functions to retrieve current organization and membership details

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,11 +1,42 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookieStore = await cookies()
   return !!cookieStore.get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  return (await cookies()).get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const currentOrg = await getCurrentOrg()
+
+  if (!currentOrg) {
+    return null
+  }
+
+  const { memberhsip } = await getMembership(currentOrg)
+  return memberhsip
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    role: membership.role,
+    id: membership.userId,
+  })
+
+  return ability
 }
 
 export async function auth() {
