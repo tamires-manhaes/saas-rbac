257ed5dc0dec9ae3397420c50b9cba469a62b27d
Tamires <tamiresmanhaes4@gmail.com>
2025-02-25 18:08:07 -0300

    feat: add theme switcher component to header for theme management

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -4,9 +4,13 @@ import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
 export async function Header() {
   const permissions = await ability()
+
+  console.log(permissions)
 
   return (
     <header className="mx-auto flex max-w-[1200px] items-center justify-between px-4">
@@ -20,6 +24,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </header>
