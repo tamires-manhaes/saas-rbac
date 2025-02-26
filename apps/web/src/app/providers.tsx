c01b43aaa60c2587b4215bd8e4118d5423501485
Tamires <tamiresmanhaes4@gmail.com>
2025-02-26 20:16:08 -0300

    feat: integrate @tanstack/react-query and refactor layout with Providers component

---
+++ apps/web/src/app/providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+
+import { queryClient } from '@/lib/react-query'
+
+export function Providers({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
