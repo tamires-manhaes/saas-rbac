618ba57642efd11691a87a3a85a54a7040a79922
Tamires <tamiresmanhaes4@gmail.com>
2025-02-26 18:19:04 -0300

    feat: add Header component to Container for improved layout structure

--- apps/web/src/components/container.tsx
+++ apps/web/src/components/container.tsx
@@ -1,5 +1,10 @@
+import { Header } from './header'
+
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto max-w-[1200px] space-y-4 px-4 py-4">{children}</div>
+    <div className="mx-auto max-w-[1200px] space-y-4 px-4 py-4">
+      <Header />
+      <>{children}</>
+    </div>
   )
 }
