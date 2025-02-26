936e8a38c20d17c507922f67648eabc33240f691
Tamires <tamiresmanhaes4@gmail.com>
2025-02-26 15:01:13 -0300

    feat: implement organization creation functionality with validation and form handling

---
+++ apps/web/src/app/(app)/create-organization/organization-form.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import { AlertTriangle, Loader2 } from 'lucide-react'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import { useFormState } from '@/hooks/use-form-state'
+
+import { createOrganizationAction } from './actions'
+
+export function OrganizationForm() {
+  const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
+    createOrganizationAction,
+    () => {},
+  )
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Save Organization failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      {success === true && message && (
+        <Alert variant="success">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Success!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="space-y-1">
+        <Label htmlFor="name">Org Name</Label>
+        <Input name="name" id="name" />
+        {errors?.name && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.name[0]}
+          </p>
+        )}
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="domain">E-mail Domain</Label>
+        <Input
+          name="domain"
+          type="text"
+          id="domain"
+          inputMode="url"
+          placeholder="example.com"
+        />
+        {errors?.domain && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.domain[0]}
+          </p>
+        )}
+      </div>
+      <div className="space-y-1">
+        <div className="flex items-baseline space-x-2">
+          <Checkbox
+            name="shouldAttachUsersByDomain"
+            id="shouldAttachUsersByDomain"
+            className="translate-y-0.5"
+          />
+          <label htmlFor="shouldAttachUsersByDomain" className="space-y-1">
+            <span className="text-sm font-medium leading-none">
+              Auto-join new members
+            </span>
+            <p className="text-sm text-muted-foreground">
+              This will automaticaly invite all members with same email domain
+              to this organization
+            </p>
+          </label>
+        </div>
+        {errors?.shouldAttachUsersByDomain && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.shouldAttachUsersByDomain[0]}
+          </p>
+        )}
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size- animate-spin" />
+        ) : (
+          'Save Organization'
+        )}
+      </Button>
+
+      <Separator />
+    </form>
+  )
+}
--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,73 +1,10 @@
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import { OrganizationForm } from './organization-form'
 
 export default function CreateOrganizationPage() {
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-bold">Create organization</h1>
-
-      <form className="space-y-4">
-        {/* {success === false && message && (
-          <Alert variant="destructive">
-            <AlertTriangle className="size-4" />
-            <AlertTitle>SIgn in failed!</AlertTitle>
-            <AlertDescription>
-              <p>{message}</p>
-            </AlertDescription>
-          </Alert>
-        )} */}
-        <div className="space-y-1">
-          <Label htmlFor="name">Org Name</Label>
-          <Input name="name" id="name" />
-          {/* {errors?.name && (
-            <p className="text-xs font-medium text-red-500 dark:text-red-400">
-              {errors.email[0]}
-            </p>
-          )} */}
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="domain">E-mail Domain</Label>
-          <Input
-            name="domain"
-            type="text"
-            id="domain"
-            inputMode="url"
-            placeholder="example.com"
-          />
-          {/* {errors?.email && (
-            <p className="text-xs font-medium text-red-500 dark:text-red-400">
-              {errors.email[0]}
-            </p>
-          )} */}
-        </div>
-        <div className="space-y-1">
-          <div className="flex items-baseline space-x-2">
-            <Checkbox
-              name="shouldAttachUsersByDomain"
-              id="shouldAttachUsersByDomain"
-              className="translate-y-0.5"
-            />
-            <label htmlFor="shouldAttachUsersByDomain" className="space-y-1">
-              <span className="text-sm font-medium leading-none">
-                Auto-join new members
-              </span>
-              <p className="text-sm text-muted-foreground">
-                This will automaticaly invite all members with same email domain
-                to this organization
-              </p>
-            </label>
-          </div>
-        </div>
-
-        <Button type="submit" className="w-full">
-          Save Organization
-        </Button>
-
-        <Separator />
-      </form>
+      <OrganizationForm />
     </div>
   )
 }
