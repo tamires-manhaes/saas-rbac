f3c331b5aa0dd3af49078470c14b0ef0a6d548f5
Tamires <tamiresmanhaes4@gmail.com>
2025-02-26 18:19:11 -0300

    refactor: remove Header component from Projects and Home pages for cleaner layout

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,10 +1,8 @@
 import Container from '@/components/container'
-import { Header } from '@/components/header'
 
 export default async function Projects() {
   return (
     <Container>
-      <Header />
       <main>Org page/Projects</main>
     </Container>
   )
--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,10 +1,8 @@
 import Container from '@/components/container'
-import { Header } from '@/components/header'
 
 export default async function Home() {
   return (
     <Container>
-      <Header />
       <main className="mx-auto w-full max-w-[1200px] space-y-4">
         <p className="text-sm text-muted-foreground">Select an organization</p>
       </main>
