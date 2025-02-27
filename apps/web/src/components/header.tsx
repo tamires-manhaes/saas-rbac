aacdd66b0bf1a3507d7f551a1f7b879033ceea99
Tamires <tamiresmanhaes4@gmail.com>
2025-02-27 18:18:41 -0300

    feat: add ProjectSwitcher to Header for enhanced project management

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -4,6 +4,7 @@ import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
+import { ProjectSwitcher } from './project-switcher'
 import { ThemeSwitcher } from './theme/theme-switcher'
 import { Separator } from './ui/separator'
 
@@ -18,7 +19,12 @@ export async function Header() {
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
         <OrganizationSwitcher />
-        {permissions?.can('get', 'Project') && <span>projects</span>}
+        {permissions?.can('get', 'Project') && (
+          <>
+            <Slash className="size-3 -rotate-[24deg] text-border" />
+            <ProjectSwitcher />
+          </>
+        )}
       </div>
 
       <div className="flex items-center gap-4">
