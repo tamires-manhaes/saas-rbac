563d2427eda0d31fc88d725d70437f657a125fc5
Tamires <tamiresmanhaes4@gmail.com>
2025-02-27 18:19:11 -0300

    feat: add getProjects function for fetching organization projects

---
+++ apps/web/src/http/get-projects.ts
@@ -0,0 +1,28 @@
+import { api } from './api-client'
+
+interface GetProjectsResponse {
+  projects: {
+    description: string
+    id: string
+    name: string
+    slug: string
+    avatarUrl: string | null
+    organizationId: string
+    ownerId: string
+    createdAt: string
+    owner: {
+      id: string
+      name: string | null
+      avatarUrl: string | null
+    }
+  }[]
+}
+
+export async function getProjects(org: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const result = await api
+    .get(`organizations/${org}/projects`)
+    .json<GetProjectsResponse>()
+  return result
+}
