4a9d98720e1888933fdbc4467cab576a395b84fc
Tamires <tamiresmanhaes4@gmail.com>
2025-02-28 13:16:56 -0300

    feat: add permission check for project creation in CreateProject component

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,8 +1,17 @@
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 import Container from '@/components/container'
 
 import { ProjectForm } from './project-form'
 
-export default function CreateProject() {
+export default async function CreateProject() {
+  const permissions = await ability()
+
+  if (!permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
   return (
     <Container>
       <h1 className="text-2xl font-bold">Create project</h1>
