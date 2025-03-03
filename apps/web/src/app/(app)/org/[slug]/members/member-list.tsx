d74614370aeea47ed86acfcb5a917945fa0d2f83
Tamires <tamiresmanhaes4@gmail.com>
2025-03-03 19:58:25 -0300

    feat: add Members page with Invites and MemberList components

---
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -0,0 +1,84 @@
+import { organizationSchema } from '@saas/auth'
+import { ArrowLeftRight, Crown } from 'lucide-react'
+import Image from 'next/image'
+
+import { ability, getCurrentOrg } from '@/auth/auth'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { getMembers } from '@/http/get-members'
+import { getMembership } from '@/http/get-membership'
+import { getOrganization } from '@/http/get-organization'
+
+export async function MemberList() {
+  const currentOrg = await getCurrentOrg()
+  const permissions = await ability()
+
+  const [{ membership }, { members }, { organization }] = await Promise.all([
+    getMembership(currentOrg!),
+    getMembers(currentOrg!),
+    getOrganization(currentOrg!),
+  ])
+
+  const authOrg = organizationSchema.parse(organization)
+
+  return (
+    <div className="sapce-y-2">
+      <h2 className="text-lg font-semibold">Members</h2>
+
+      <div className="rounded border">
+        <Table>
+          <TableBody>
+            {members.map((member) => {
+              return (
+                <TableRow key={member.id}>
+                  <TableCell className="py-2.5" style={{ width: 48 }}>
+                    <Avatar>
+                      <AvatarFallback />
+                      {member.avatarUrl && (
+                        <Image
+                          src={member.avatarUrl}
+                          width={32}
+                          height={32}
+                          alt=""
+                          className="aspect-square size-full"
+                        />
+                      )}
+                    </Avatar>
+                  </TableCell>
+                  <TableCell className="py-2.5">
+                    <div className="flex flex-col">
+                      <span className="inline-flex items-center gap-2 font-medium">
+                        {member.name}
+                        {member.userId === membership.userId && ' (me)'}
+                        {organization.ownerId === member.userId && (
+                          <span className="items-centergap-1 inline-flex text-xs text-muted-foreground">
+                            <Crown className="size-3" />
+                            owner
+                          </span>
+                        )}
+                      </span>
+                      <span className="text-xs text-muted-foreground">
+                        {member.email}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="py-2.5">
+                    <div className="flex items-center justify-end gap-2">
+                      {permissions?.can('transfer_ownership', authOrg) && (
+                        <Button size="sm" variant="ghost">
+                          <ArrowLeftRight className="mr-2 size-4" />
+                          transfer ownership
+                        </Button>
+                      )}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
