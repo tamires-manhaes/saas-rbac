75ef42db4a0ea272ab3c4b41841e44f9ddd7c628
Tamires <tamiresmanhaes4@gmail.com>
2025-03-03 15:15:32 -0300

    fix: update import paths for OrganizationForm component in create-organization pages

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,6 +1,6 @@
 import Container from '@/components/container'
 
-import { OrganizationForm } from './organization-form'
+import { OrganizationForm } from '../org/organization-form'
 
 export default function CreateOrganization() {
   return (
