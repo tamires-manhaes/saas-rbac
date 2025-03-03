0936d360362281b80e4433514c343a576178356d
Tamires <tamiresmanhaes4@gmail.com>
2025-03-03 19:26:53 -0300

    refactor: remove unused Separator component from OrganizationForm

--- apps/web/src/app/(app)/org/organization-form.tsx
+++ apps/web/src/app/(app)/org/organization-form.tsx
@@ -7,7 +7,6 @@ import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
 import { useFormState } from '@/hooks/use-form-state'
 
 import { createOrganizationAction, updateOrganizationAction } from './actions'
@@ -109,7 +108,6 @@ export function OrganizationForm({
           'Save Organization'
         )}
       </Button>
-      <Separator />
     </form>
   )
 }
