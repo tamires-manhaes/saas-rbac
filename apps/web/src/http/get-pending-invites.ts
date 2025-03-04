a19a9ec56f7dd2a43c0fa7718bba061facc32254
Tamires <tamiresmanhaes4@gmail.com>
2025-03-04 16:46:57 -0300

    feat: add functions to decline invites and retrieve pending invites

---
+++ apps/web/src/http/get-pending-invites.ts
@@ -0,0 +1,27 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetPendingInvitesResponse {
+  invites: {
+    id: string
+    role: Role
+    email: string
+    createdAt: string
+    organization: {
+      name: string
+    }
+    author: {
+      id: string
+      name: string | null
+      avatarUrl: string | null
+    } | null
+  }[]
+}
+
+export async function getPendingInvites() {
+  const result = await api
+    .get(`pending-invites`)
+    .json<GetPendingInvitesResponse>()
+  return result
+}
